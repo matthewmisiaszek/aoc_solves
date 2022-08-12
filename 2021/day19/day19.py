d3a5a9dfc5d77d39d8764feda09d8035543196b4
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-08-12 11:51:44 -0400

    Improvements, day 19

--- 2021/day19/day19.py
+++ 2021/day19/day19.py
@@ -1,8 +1,9 @@
 import time
+
 start = time.time()
 
-
 import numpy as np
+
 
 class ScanClass:
     def __init__(self, scanner):
@@ -18,37 +19,41 @@ class ScanClass:
         lines = {}
         for ai in range(len(self.beacons)):
             a = self.beacons[ai]
-            for bi in range(ai+1,len(self.beacons)):
+            for bi in range(ai + 1, len(self.beacons)):
                 b = self.beacons[bi]
-                lines[(ai, bi)] = sum([(ai - bi) ** 2 for ai, bi in zip(a,b)]) ** .5
+                lines[(ai, bi)] = sum(np.subtract(a, b) ** 2) ** .5
         self.triangles = {}
         for ai in range(len(self.beacons)):
-            for bi in range(ai+1,len(self.beacons)):
-                for ci in range(bi+1, len(self.beacons)):
+            for bi in range(ai + 1, len(self.beacons)):
+                for ci in range(bi + 1, len(self.beacons)):
                     sides = [lines[(ai, bi)], lines[(bi, ci)], lines[(ai, ci)]]
-                    corners = [ci,ai, bi]
+                    corners = [ci, ai, bi]
                     while not sides[0] == min(sides):
                         sides = sides[1:] + [sides[0]]
                         corners = corners[1:] + [corners[0]]
                     if sides[1] > sides[2]:
                         sides = [sides[0], sides[2], sides[1]]
                         corners = [corners[0], corners[2], corners[1]]
-                    if len(sides)==len(set(sides)):
-                        self.triangles[tuple(sides)]=tuple(corners)
+                    if len(sides) == len(set(sides)):
+                        self.triangles[tuple(sides)] = tuple(corners)
 
-    def match_beacon(self,other_beacon):
+    def match_scanner(self, other_scanner):
         my_triangles = set(self.triangles.keys())
-        other_triangles = set(other_beacon.triangles.keys())
+        other_triangles = set(other_scanner.triangles.keys())
         match_triangles = list(my_triangles.intersection(other_triangles))
         if match_triangles:
             match_triangle = match_triangles[0]
-            m = np.array([np.array(self.beacons[i]) for i in self.triangles[match_triangle]])
-            o = np.array([np.array(other_beacon.beacons[i]) for i in other_beacon.triangles[match_triangle]])
-            mavg = m[0]+(m[1]-m[0])/np.abs(m[1]-m[0]).astype(int)
-            oavg = o[0]+(o[1]-o[0])/np.abs(o[1]-o[0]).astype(int)
-            A = np.round(np.linalg.solve((m-mavg), (o-oavg)),0).astype(int)
-            self.beacons = [tuple((beacon-mavg)@A + oavg) for beacon in self.beacons]
-            self.position = mavg@A-oavg
+            matrices, offsets = [], []
+            for scanner in (self, other_scanner):
+                a, b, c = [scanner.beacons[i] for i in scanner.triangles[match_triangle]]
+                b, c = [np.subtract(i, a) for i in (b, c)]
+                d = np.cross(b, c)
+                matrices.append((b, c, d))
+                offsets.append(a)
+            A = np.round(np.linalg.solve(*matrices), 0).astype(int)
+            O = offsets[0] @ A - offsets[1]
+            self.beacons = [tuple(beacon @ A - O) for beacon in self.beacons]
+            self.position = O
             return True
         else:
             return False
@@ -57,21 +62,21 @@ class ScanClass:
 def main(input_file='input.txt', verbose=False):
     scanraw = open(input_file).read().split('\n\n')
     scanners = [ScanClass(scanner) for scanner in scanraw]
-    scanners[0].position = (0,0,0)
+    scanners[0].position = (0, 0, 0)
     matched = set([0])
-    while len(matched)<len(scanners):
+    while len(matched) < len(scanners):
         for i in range(len(scanners)):
             if i not in matched:
                 for j in matched:
-                    if scanners[i].match_beacon(scanners[j]):
+                    if scanners[i].match_scanner(scanners[j]):
                         matched.add(i)
                         break
     beacons = set()
-    maxman=0
+    maxman = 0
     for scanner in scanners:
         beacons = beacons.union(set(scanner.beacons))
         for scanner2 in scanners:
-            man = sum(abs(ai-bi) for ai,bi in zip(scanner.position, scanner2.position))
+            man = int(sum(abs(ai - bi) for ai, bi in zip(scanner.position, scanner2.position)))
             maxman = max(maxman, man)
     p1 = len(beacons)
     p2 = maxman
@@ -85,4 +90,4 @@ def main(input_file='input.txt', verbose=False):
 
 if __name__ == "__main__":
     main(verbose=True)
-    print('elapsed time: ',time.time()-start)
+    print('elapsed time: ', time.time() - start)
