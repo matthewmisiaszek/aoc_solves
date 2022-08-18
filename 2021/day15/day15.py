cc5b205127b9bd5baa19deba140fb70386f5fbb7
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-08-17 23:35:48 -0400

    Day 15 update

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -1,70 +1,42 @@
 from queue import PriorityQueue
 
 
-def merge_two_dicts(x, y):
-    z = x.copy()  # start with keys and values of x
-    z.update(y)  # modifies z with keys and values of y
-    return z
-
-
 def shortest_path(start, end, cave):
     q = PriorityQueue()
     q.put((0, start))
     while not q.empty():
         dist, loc = q.get()
-        for n in neighbors(cave, loc):
-            if n == end:
-                return dist + cave[n]
-            q.put((dist + cave[n], n))
-            cave.pop(n)
+        for offset in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+            n = (loc[0] + offset[0], loc[1] + offset[1])
+            if n in cave:
+                if n == end:
+                    return dist + cave[n]
+                q.put((dist + cave[n], n))
+                cave.pop(n)
 
 
-def makegrid(f):
-    grid = {}
-    for y, line in enumerate(f):
-        for x, c in enumerate(line):
-            grid[(x, y)] = int(c)
-    return grid
-
-
-def neighbors(grid, point):
-    ret = []
-    for offset in ((0, 1), (1, 0), (0, -1), (-1, 0)):
-        new_point = (point[0] + offset[0], point[1] + offset[1])
-        if new_point in grid:
-            ret.append(new_point)
-    return ret
-
-
-def bounds(grid):
-    minbound = tuple([min(grid.keys(), key=lambda x: x[i])[i] for i in (0, 1)])
-    maxbound = tuple([max(grid.keys(), key=lambda x: x[i])[i] for i in (0, 1)])
-    return minbound, maxbound
-
-
-def expand(cave):
-    repeat = 5
-    ubound = bounds(cave)[1]
-    for key in tuple(cave.keys()):
-        val = cave[key]
+def expand(cave, repeat=5):
+    ux, uy = max(cave.keys())
+    ux, uy = ux + 1, uy + 1
+    for (kx, ky), val in tuple(cave.items()):
         for x in range(repeat):
             for y in range(repeat):
-                ngrid = (x, y)
-                if not ngrid == (0, 0):
-                    nkey = tuple(key[i] + (ubound[i] + 1) * ngrid[i] for i in range(2))
+                if x or y:
+                    nkey = (kx + ux * x, ky + uy * y)
                     nval = (val + x + y - 1) % 9 + 1
                     cave[nkey] = nval
 
 
 def main(input_file='input.txt', verbose=False):
     f = open(input_file).read().split('\n')
-    cave = makegrid(f)
-    ubound = bounds(cave)[1]
+    cave = {}
+    for y, line in enumerate(f):
+        for x, c in enumerate(line):
+            cave[(x, y)] = int(c)
     cave2 = cave.copy()
-    p1 = shortest_path((0, 0), ubound, cave)
+    p1 = shortest_path((0, 0), max(cave.keys()), cave)
     expand(cave2)
-    ubound = bounds(cave2)[1]
-    p2 = shortest_path((0, 0), ubound, cave2)
+    p2 = shortest_path((0, 0), max(cave2.keys()), cave2)
 
     if verbose:
         print('Part 1: {0[0]}\nPart 2: {0[1]}'.format([p1, p2]))
