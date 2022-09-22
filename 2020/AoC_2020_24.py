47fda66bf93f9fbaaf261ea977ea29ff62b24b2f
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-09-22 10:49:01 -0400

    Add files via upload

---
+++ 2020/AoC_2020_24.py
@@ -0,0 +1,59 @@
+from common2020 import aoc_input
+from common.timer import timer
+from common import elementwise as ew
+from collections import Counter
+
+
+def install_tiles(input_string, directions, translate):
+    for a, b in translate:  # replace 2 character directions with 1 character substitutions
+        input_string = input_string.replace(a, b)
+    tiles = set()
+    for instruction in input_string.split('\n'):
+        tile = (0, 0)
+        for key, val in directions.items():  # count instances of each direction in instruction and offset
+            tile = ew.sum2d(tile, val, instruction.count(key))
+        if tile in tiles:  # flip the tile (in set means black, not in set means white)
+            tiles.discard(tile)
+        else:
+            tiles.add(tile)
+    return tiles
+
+
+def simulate_day(tiles, directions):
+    adj_list = []  # list of tiles adjacent to every black tile in tiles including duplicates
+    for tile in tiles:
+        for direction in directions.values():
+            adj = ew.sum2d(tile, direction)
+            adj_list.append(adj)
+    adj_count = Counter(adj_list)  # count occurrences of each adjacent tile
+    tiles &= adj_count.keys()  # remove black tiles with no adjacent tiles
+    for tile in tiles.copy():  # remove black tiles with >2 adjacent tiles
+        if adj_count[tile] > 2:
+            tiles.discard(tile)
+    for tile in adj_count.keys() - tiles:  # add black tiles if white tile has ==2 adjacents
+        if adj_count[tile] == 2:
+            tiles.add(tile)
+
+    return tiles
+
+
+def main(input_string, verbose=False):
+    # replace two character directions with 1 character substitute
+    # optional: replace one character direction with substitute for consistency
+    # convention: East is 0, other directions numbered CCW from East
+    translate = (('ne', '1'), ('nw', '2'), ('sw', '4'), ('se', '5'), ('e', '0'), ('w', '3'))
+    # convention: East is (1,0), Northeast is (0,1)
+    directions = {'0': (1, 0), '1': (0, 1), '2': (-1, 1), '3': (-1, 0), '4': (0, -1), '5': (1, -1)}
+    tiles = install_tiles(input_string, directions, translate)
+    p1 = len(tiles)
+    for day in range(100):
+        tiles = simulate_day(tiles, directions)
+    p2 = len(tiles)
+    if verbose:
+        print('Part 1: {0[0]}\nPart 2: {0[1]}'.format([p1, p2]))
+    return p1, p2
+
+
+if __name__ == "__main__":
+    main(aoc_input(2020, 24), verbose=True)
+    print('Time:  ', timer())
---
+++ 2020/AoC_2020_25.py
@@ -0,0 +1,30 @@
+from common2020 import aoc_input
+from common.timer import timer
+from common.holiday_greeting import holiday_greeting
+
+
+def main(input_string, verbose=False):
+    card_pkey, door_pkey = [int(i) for i in input_string.split('\n')]
+    subject_number = 7
+    modfact = 20201227
+    pkey = 1
+    card_loops = 0
+    while pkey != card_pkey:
+        pkey *= subject_number
+        pkey %= modfact
+        card_loops += 1
+    ekey = 1
+    subject_number = door_pkey
+    for loop in range(card_loops):
+        ekey *= subject_number
+        ekey %= modfact
+    p1 = ekey
+    p2 = holiday_greeting
+    if verbose:
+        print('Part 1: {0[0]}\nPart 2: {0[1]}'.format([p1, p2]))
+    return p1, p2
+
+
+if __name__ == "__main__":
+    main(aoc_input(2020, 25), verbose=True)
+    print('Time:  ', timer())
