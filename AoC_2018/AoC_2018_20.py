3a2fcbf034c46d5ba7c91c1f45c4f26fd557598b
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-11-09 09:12:31 -0500

    update common.graph

--- AoC_2018/AoC_2018_20.py
+++ AoC_2018/AoC_2018_20.py
@@ -36,7 +36,8 @@ def re_path(input_string):
 
 def main(input_string, verbose=False):
     doors = re_path(input_string)
-    p1, paths = doors.dijkstra((0, 0), doors.graph.keys(), all_paths=True)
+    paths = doors.dijkstra((0, 0), doors.graph.keys(), all_paths=True)
+    p1 = max(paths.values())
     target_length = 1000
     p2 = sum([value > target_length for value in paths.values()])
     return p1, p2
