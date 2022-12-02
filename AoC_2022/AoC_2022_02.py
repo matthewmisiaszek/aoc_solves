22f2e187e939e5c9398b64d838e058280e432aa1
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-12-02 01:31:37 -0500

    2022 Day 2 v2

--- AoC_2022/AoC_2022_02.py
+++ AoC_2022/AoC_2022_02.py
@@ -4,42 +4,26 @@ ABC = 'ABC'
 XYZ = 'XYZ'
 
 
-def part1(guide):
-    score = 0
-    for line in guide.split('\n'):
-        a, b = line.split()
-        opp = ABC.find(a)
-        you = XYZ.find(b)
-        score += you + 1
-        if opp == you:  # draw
-            score += 3
-        elif (you - opp) % 3 == 1:  # win
-            score += 6
-    return score
+def part1(opp, you):
+    out = (you - opp + 1) % 3
+    return you + 1 + out * 3
 
 
-def part2(guide):
-    score = 0
-    for line in guide.split('\n'):
-        a, b = line.split()
-        opp = ABC.find(a)
-        outcome = XYZ.find(b)
-        match outcome:
-            case 0:  # lose
-                score += 0
-                score += (opp - 1) % 3 + 1
-            case 1:  # draw
-                score += 3
-                score += opp + 1
-            case 2:  # win
-                score += 6
-                score += (opp + 1) % 3 + 1
-    return score
+def part2(opp, out):
+    you = (opp + out - 1) % 3
+    return out * 3 + you + 1
+
+
+def parse(input_string):
+    split_string = [line.split() for line in input_string.split('\n')]
+    guide = [(ABC.find(a), XYZ.find(b)) for a, b in split_string]
+    return guide
 
 
 def main(input_string, verbose=False):
-    p1 = part1(input_string)
-    p2 = part2(input_string)
+    guide = parse(input_string)
+    p1 = sum(part1(opp, you) for opp, you in guide)
+    p2 = sum(part2(opp, out) for opp, out in guide)
     return p1, p2
 
 
