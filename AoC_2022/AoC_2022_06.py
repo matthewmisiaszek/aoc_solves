5aa8d1d529c699cc8a0f630d48575b4dc81f661e
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-12-06 00:07:01 -0500

    2022 day 6

---
+++ AoC_2022/AoC_2022_06.py
@@ -0,0 +1,17 @@
+import dancer
+
+
+def find_sequence(datastream, characters):
+    for i in range(len(datastream)):
+        if len(set(datastream[i:i + characters])) == characters:
+            return i + characters
+
+
+def main(input_string, verbose=False):
+    p1 = find_sequence(input_string, 4)
+    p2 = find_sequence(input_string, 14)
+    return p1, p2
+
+
+if __name__ == "__main__":
+    dancer.run(main, year=2022, day=6, verbose=True)
