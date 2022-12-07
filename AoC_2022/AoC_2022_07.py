7165b42c44231199e1c93a392cde306721ce1831
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-12-07 09:16:34 -0500

    2022 day 7

---
+++ AoC_2022/AoC_2022_07.py
@@ -0,0 +1,48 @@
+import dancer
+import re
+
+ROOT = '/'
+FS_SIZE = 70000000
+NEED_SIZE = 30000000
+P1SIZE = 100000
+PARENT = '..'
+CDPAT = r'\$ cd (.*)'
+FPAT = r'(\d+).*'
+
+
+def main(input_string, verbose=False):
+    root_path = (ROOT,)
+    path = root_path
+    directories = {path: 0}
+    for line in input_string.split('\n'):
+        d = re.search(CDPAT, line)
+        if d:
+            d = d.group(1)
+            if d == PARENT:
+                path = path[:-1]
+            elif d == ROOT:
+                path = root_path
+            else:
+                path += (d,)
+                if path not in directories:
+                    directories[path] = 0
+        else:
+            f = re.search(FPAT, line)
+            if f:
+                directories[path] += int(f.group(1))
+
+    for directory in sorted(directories.keys(), reverse=True):
+        if len(directory) > 1:
+            directories[directory[:-1]] += directories[directory]
+
+    p1 = sum(directory for directory in directories.values() if directory <= P1SIZE)
+
+    need = NEED_SIZE - FS_SIZE + directories[root_path]
+    for p2 in sorted(directories.values()):
+        if p2 >= need:
+            break
+    return p1, p2
+
+
+if __name__ == "__main__":
+    dancer.run(main, year=2022, day=7, verbose=True)
