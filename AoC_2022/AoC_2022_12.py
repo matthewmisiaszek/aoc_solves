1b8f10c442e339eb2e3670cf4331c8906b2c1d08
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2022-12-12 00:26:26 -0500

    2022 day 12

---
+++ AoC_2022/AoC_2022_12.py
@@ -0,0 +1,28 @@
+import dancer
+from common import graph, bfsqueue, constants as con, elementwise as ew
+from string import ascii_lowercase
+
+START_KEY = 'S'
+END_KEY = 'E'
+START_H = 'a'
+END_H = 'z'
+
+
+def main(input_string, verbose=False):
+    amap = graph.text_to_dict(input_string)  # map of (x,y):c
+    poi = {val: key for key, val in amap.items()}  # map of c:(x,y) to find S, E
+    amap.update({poi[START_KEY]: START_H, poi[END_KEY]: END_H})  # replace S, E with their heights
+    nmap = {key: ascii_lowercase.find(val) for key, val in amap.items()}  # substitute letters for int
+    heighmap = graph.set_to_graph(nmap.keys())  # graph ignoring elevation
+    for point, neighbors in heighmap.graph.items():  # delete neighbors that require climbing gear
+        for neighbor in tuple(neighbors.keys()):
+            if nmap[neighbor] - nmap[point] > 1:
+                neighbors.pop(neighbor)
+    p1 = heighmap.simple_dijkstra(poi[START_KEY], poi[END_KEY])
+    low_points = {key for key, val in amap.items() if val == START_H}  # set of points at start height
+    p2 = heighmap.dijkstra(low_points, {poi[END_KEY]})[poi[END_KEY]]
+    return p1, p2
+
+
+if __name__ == "__main__":
+    dancer.run(main, year=2022, day=12, verbose=True)
