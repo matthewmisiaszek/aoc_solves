34001e6f12c1a0378f8453d9a7947cca945f8e5b
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2023-12-08 07:48:42 +0100

    2023 day 8

---
+++ AoC_2023/AoC_2023_08.py
@@ -0,0 +1,42 @@
+import dancer
+import math
+
+
+INSTRUCTIONS = 'LR'
+A, Z = 'A', 'Z'
+AAA, ZZZ = 'AAA', 'ZZZ'
+
+
+def parse(input_string):
+    instructions, network_list = input_string.split('\n\n')
+    instructions = tuple(INSTRUCTIONS.index(i) for i in instructions)
+    network = {}
+    for line in network_list.split('\n'):
+        node, children = line.split(' = ')
+        l, r = children[1:-1].split(', ')
+        network[node] = (l, r)
+    return instructions, network
+
+
+def navigate(instructions, network, start, finish):
+    node = start
+    i = 0
+    while node not in finish:
+        inst = instructions[i%len(instructions)]
+        node = network[node][inst]
+        i += 1
+    return i
+
+
+def main(input_string, verbose=False):
+    instructions, network = parse(input_string)
+    p1 = navigate(instructions, network, AAA, {ZZZ})
+    anodes = {node for node in network if node[-1] == A}
+    znodes = {node for node in network if node[-1] == Z}
+    frequencies = [navigate(instructions, network, node, znodes) for node in anodes]
+    p2 = math.lcm(*frequencies)
+    return p1, p2
+
+
+if __name__ == "__main__":
+    dancer.run(main, year=2023, day=8, verbose=True)
