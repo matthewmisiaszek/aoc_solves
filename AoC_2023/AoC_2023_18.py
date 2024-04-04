5bcb00b6e7fef0ba7bcce28a053bdf645a550318
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2024-04-04 23:31:31 +0200

    update 2023 day 18

--- AoC_2023/AoC_2023_18.py
+++ AoC_2023/AoC_2023_18.py
@@ -1,58 +1,38 @@
 import dancer
-from common import spatial
-from collections import defaultdict
-from itertools import product
 
 
-def intersection_length(cranges, nranges):
-    il = 0
-    for (ca, cb), (na, nb) in product(cranges, nranges):
-        ia = max(ca, na)
-        ib = min(cb, nb)
-        if ib >= ia:
-            il += (ib - ia + 1)
-    return il
-
-
-def dig(instructions):
-    digger = spatial.Point(0, 0)
-    lines = defaultdict(set)
-    for direction, distance in instructions:
-        digger += direction * distance
-        lines[digger.y].add(digger.x)
-    return fill(lines)
-
-
-def fill(lines):
-    full = 0
-    current, cranges = set(), tuple()
-    csum, cy = 0, min(lines)-1
-    for y in sorted(lines):
-        full += csum * (y - cy + 1)
-        new = current ^ lines[y]
-        nsort = sorted(new)
-        nranges = tuple((a, b) for a, b in zip(nsort[::2], nsort[1::2]))
-        full -= intersection_length(cranges, nranges)
-        csum = sum((b-a+1) for a, b in nranges)
-        current, cranges, cy = new, nranges, y
-    return full
+def dig(dists, heads):
+    heads = heads[-1:] + heads + heads[:1]
+    cw = len(heads) // 2 < sum((a + 1) % 4 == b for a, b in zip(heads, heads[1:]))
+    if not cw:
+        # this solution only works if the digger is going CW
+        # if it's going CCW, mirror the input
+        flip = {0: 2, 2: 0, 1: 1, 3: 3}
+        heads = [flip[i] for i in heads]
+    ypos = area = 0
+    for head_prev, head, head_next, dist in zip(heads, heads[1:], heads[2:], dists):
+        match head:
+            case 3:
+                ypos += dist
+            case 1:
+                ypos -= dist
+            case 0:
+                dist += -1 + (head_prev == 3) + (head_next == 1)
+                area += (ypos + 1) * dist
+            case 2:
+                dist += -1 + (head_prev == 1) + (head_next == 3)
+                area -= ypos * dist
+    return area
 
 
 def main(input_string, verbose=False):
-    p1_instructions = []
-    p2_instructions = []
-    directions = (spatial.EAST, spatial.SOUTH, spatial.WEST, spatial.NORTH)
-    letters = 'RDLU'
-    ddict = {a: b for a, b in zip(letters, directions)}
-    for line in input_string.split('\n'):
-        direction, distance, hexcode = line.split()
-        direction = ddict[direction]
-        p1_instructions.append((direction, int(distance)))
-        direction = directions[int(hexcode[-2])]
-        distance = int(hexcode[2:-2], 16)
-        p2_instructions.append((direction, distance))
-    p1 = dig(p1_instructions)
-    p2 = dig(p2_instructions)
+    heads, dists, c = zip(*(line.split() for line in input_string.split('\n')))
+    head_dict = {'R': 0, 'D': 1, 'L': 2, 'U': 3}
+    heads = [head_dict[i] for i in heads]
+    dists = [int(i) for i in dists]
+    p1 = dig(dists, heads)
+    dists, heads = zip(*((int(hexc[2:7], 16), int(hexc[7])) for hexc in c))
+    p2 = dig(dists, heads)
     return p1, p2
 
 
