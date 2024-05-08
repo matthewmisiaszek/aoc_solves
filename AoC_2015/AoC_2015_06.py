8cf236247f50daedb9923510322f938003ff8a56
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2024-05-08 16:47:07 +0200

    improve speed 2015:06 with numpy

--- AoC_2015/AoC_2015_06.py
+++ AoC_2015/AoC_2015_06.py
@@ -1,31 +1,31 @@
 import blitzen
 import re
+import numpy as np
 
 
 def main(input_string, verbose=False):
     size = 1000
     pattern = r'(.*) (\d*),(\d*) through (\d*),(\d*)'
-    p1 = {(x, y): 0 for x in range(size) for y in range(size)}
+    zero = np.zeros((size, size))
+    one = np.full(zero.shape, 1)
+    p1 = zero.copy()
     p2 = p1.copy()
     for line in input_string.split('\n'):
         command, x1, y1, x2, y2 = re.match(pattern, line).groups()
         x1, y1, x2, y2 = (int(i) for i in (x1, y1, x2, y2))
-        affected = {(x, y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)}
+        affected = (slice(x1, x2 + 1), slice(y1, y2 + 1))
         if command == 'turn on':
-            for light in affected:
-                p1[light] = 1
-                p2[light] += 1
+            p1[affected] = 1
+            p2[affected] += 1
         elif command == 'turn off':
-            for light in affected:
-                p1[light] = 0
-                if p2[light] > 0:
-                    p2[light] -= 1
+            p1[affected] = 0
+            p2[affected] -= 1
+            p2 = np.maximum(p2, zero)
         elif command == 'toggle':
-            for light in affected:
-                p1[light] = 1 - p1[light]
-                p2[light] += 2
-    p1 = sum(p1.values())
-    p2 = sum(p2.values())
+            p1[affected] = one[affected] - p1[affected]
+            p2[affected] += 2
+    p1 = int(p1.sum())
+    p2 = int(p2.sum())
     return p1, p2
 
 
