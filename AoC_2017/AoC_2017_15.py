0c1f00a8ad0e98170964ef44f2ab7dab7c5f1e48
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2024-05-08 14:51:13 +0200

    add numba.njit to 2017:15 for speed

--- AoC_2017/AoC_2017_15.py
+++ AoC_2017/AoC_2017_15.py
@@ -1,6 +1,8 @@
 import blitzen
+from numba import njit
 
 
+@njit
 def generator(value, factor, modulus, judge_modulus):
     while True:
         value *= factor
@@ -8,6 +10,7 @@ def generator(value, factor, modulus, judge_modulus):
         yield value & judge_modulus
 
 
+@njit
 def picky_generator(value, factor, modulus, judge_modulus, picky_modulus):
     while True:
         value *= factor
@@ -16,15 +19,23 @@ def picky_generator(value, factor, modulus, judge_modulus, picky_modulus):
             yield value & judge_modulus
 
 
-def main(input_string, verbose=False):
-    value_a, value_b = [int(i) for i in (line.split()[-1] for line in input_string.split('\n'))]
+@njit
+def solve(value_a, value_b):
     a = generator(value=value_a, factor=16807, modulus=2147483647, judge_modulus=0xffff)
     b = generator(value=value_b, factor=48271, modulus=2147483647, judge_modulus=0xffff)
-    p1 = sum(next(a) == next(b) for _ in range(40 * 10 ** 6))
+    p1 = p2 = 0
+    for _ in range(40 * 10 ** 6):
+        p1 += next(a) == next(b)
     a = picky_generator(value=value_a, factor=16807, modulus=2147483647, judge_modulus=0xffff, picky_modulus=4)
     b = picky_generator(value=value_b, factor=48271, modulus=2147483647, judge_modulus=0xffff, picky_modulus=8)
-    p2 = sum(next(a) == next(b) for _ in range(5 * 10 ** 6))
+    for _ in range(5 * 10 ** 6):
+        p2 += next(a) == next(b)
     return p1, p2
+
+
+def main(input_string, verbose=False):
+    value_a, value_b = [int(i) for i in (line.split()[-1] for line in input_string.split('\n'))]
+    return solve(value_a, value_b)
 
 
 if __name__ == "__main__":
