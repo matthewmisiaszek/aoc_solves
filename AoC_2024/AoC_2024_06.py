0313793c5fd41eca1aecdddd8f5ad7eedd08d023
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2024-12-08 21:03:18 +0100

    2024 day 6 optimize, remove multiprocessing

--- AoC_2024/AoC_2024_06.py
+++ AoC_2024/AoC_2024_06.py
@@ -1,34 +1,8 @@
 import blitzen
 from donner import graph, spatial
-import multiprocessing
 
 
-def part1(bounds, obstacles, guard, heading):
-    visited = set()
-    while spatial.inbounds(guard, bounds):
-        if guard in obstacles:
-            guard -= heading
-            heading = heading.right()
-        visited.add(guard)
-        guard += heading
-    return visited
-
-
-def part2(bounds, obstacles, guard, heading):
-    visited = set()
-    while spatial.inbounds(guard, bounds):
-        if guard in obstacles:
-            guard -= heading
-            heading = heading.right()
-        if (guard, heading) in visited:
-            return True
-        visited.add((guard, heading))
-        guard += heading
-    return False
-
-
-@blitzen.run
-def main(input_string, verbose=False):
+def parse(input_string):
     grid = graph.text_to_dict(input_string)
     bounds = spatial.bounds(grid)
     obstacles = {point for point, c in grid.items() if c == '#'}
@@ -37,9 +11,36 @@ def main(input_string, verbose=False):
             guard = point
             heading = spatial.NAMES_2D[c]
             break
-    p1visited = part1(bounds, obstacles, guard, heading)
-    p1 = len(p1visited)
-    with multiprocessing.Pool(8) as pool:
-        iterable = tuple((bounds, obstacles | {i}, guard, heading) for i in p1visited)
-        p2 = sum(pool.starmap_async(part2, iterable).get())
+    return bounds, obstacles, guard, heading
+
+
+def check_loops(obstacles, bounds, guard, heading, trail):
+    while spatial.inbounds(guard, bounds):
+        if (guard, heading) in trail:
+            return True
+        trail.add((guard, heading))
+        while guard + heading in obstacles:
+            heading = heading.right()
+        guard += heading
+    return False
+
+
+@blitzen.run
+def main(input_string, verbose=False):
+    bounds, obstacles, guard, heading = parse(input_string)
+    p1, p2, trail = set(), set(), set()
+    while spatial.inbounds(guard, bounds):
+        p1.add(guard)
+        while guard + heading in obstacles:
+            heading = heading.right()
+        new_guard = guard + heading
+        if all((
+            new_guard not in p1,
+            spatial.inbounds(new_guard, bounds),
+            check_loops(obstacles | {new_guard}, bounds, guard, heading, trail.copy()),
+        )):
+            p2.add(new_guard)
+        trail.add((guard, heading))
+        guard = new_guard
+    p1, p2 = len(p1), len(p2)
     return p1, p2
