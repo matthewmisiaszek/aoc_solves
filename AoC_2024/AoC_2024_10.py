5eea257bb0e9aef36f939726447f528cfb534b20
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2024-12-11 05:56:50 +0100

    2027 day 10 update

--- AoC_2024/AoC_2024_10.py
+++ AoC_2024/AoC_2024_10.py
@@ -2,16 +2,6 @@ import blitzen
 from donner import spatial, graph
 
 
-def dfs(tgraph, point, nines, trail=()):
-    if point in nines:
-        return 1
-    ret = 0
-    for n in tgraph[point]:
-        if n not in trail:
-            ret += dfs(tgraph, n, nines, trail + (n,))
-    return ret
-
-
 @blitzen.run
 def main(input_string, verbose=False):
     topodict = graph.text_to_dict(input_string, exclude='.')
@@ -23,7 +13,16 @@ def main(input_string, verbose=False):
         for d in spatial.ENWS:
             if point + d in topodict and topodict[point] + 1 == topodict[point + d]:
                 topograph.add_edge_neq(point, point+d)
-    p1 = sum(len(topograph.dijkstra(point, nines, all_paths=True)) for point in zeros)
-    p2 = sum(dfs(topograph, point, nines) for point in zeros)
+    p1 = p2 = 0
+    for point in zeros:
+        visited = set()
+        q = [point]
+        while q:
+            c = q.pop(-1)
+            if c in nines:
+                p2 += 1
+                visited.add(c)
+            q.extend(topograph[c].keys())
+        p1 += len(visited)
     return p1, p2
 
