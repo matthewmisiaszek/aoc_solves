90cb89074bc43a2a66c4317d337720882a85171d
matthewmisiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2024-12-21 05:53:21 +0100

    Imporoved noise detection for 2024 day 14

--- AoC_2024/AoC_2024_14.py
+++ AoC_2024/AoC_2024_14.py
@@ -20,11 +20,12 @@ class Bot:
             return 0
         x = self.p.x < m.x
         y = self.p.y < m.y
-        return (x,y)
+        return x, y
 
 
 def solve(bots, bounds):
     seconds = 0
+    noise_threshold = len(bots) // 2
     while True:
         seconds += 1
         for bot in bots:
@@ -35,20 +36,15 @@ def solve(bots, bounds):
                 quadrants.pop(0)
             p1 = prod(quadrants.values())
         botset = {bot.p for bot in bots}
-        for x in range(bounds.x):
-            line = 0
-            for y in range(0, bounds.y):
-                if spatial.Point(x, y) in botset:
-                    line += 1
-                else:
-                    line = 0
-                if line > 10:
-                    return p1, seconds
+        noise = sum(bot + spatial.WEST not in botset for bot in botset)
+        if noise < noise_threshold:
+            break
+    return p1, seconds
 
 
 @blitzen.run
 def main(input_string, verbose=False):
-    bounds = spatial.Point(101,103,1)
+    bounds = spatial.Point(101, 103, 1)
     bots = [
         Bot((px, py), (vx, vy), bounds)
         for px, py, vx, vy in re.findall(r'p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)', input_string)
@@ -57,4 +53,3 @@ def main(input_string, verbose=False):
     if verbose:
         printer.printset({bot.p for bot in bots})
     return p1, p2
-
