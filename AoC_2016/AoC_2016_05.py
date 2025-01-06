4f20399e01e09e8b95b414aa06143ffbc1d8cb7d
Matt Misiaszek <76763420+matthewmisiaszek@users.noreply.github.com>
2025-01-06 22:30:55 +0100

    remove multiprocessing
    remove multiprocessing from 2016 05 and 14 because it was buggy

--- AoC_2016/AoC_2016_05.py
+++ AoC_2016/AoC_2016_05.py
@@ -1,49 +1,23 @@
 import blitzen
 from donner.misc import md5hash
-from donner import multiproc
-
-
-START_MATCH = '00000'
-START_MATCH_L = len(START_MATCH)
-PWLEN = 8
-FIVE = 5
-SEVEN = 7
-PH_CHAR = '*'
-
-
-def hash_filter(start, n, seed):
-    hashes = tuple(md5hash(seed + str(i)) for i in range(start, start+n))
-    h57 = tuple(h[FIVE:SEVEN] for h in hashes if h[:START_MATCH_L] == START_MATCH)
-    return h57
-
-
-def pw_fill(mpc, result, p1, p2, pc, verbose):
-    for h6, h7 in result:
-        if pc[0] < PWLEN:
-            p1[pc[0]] = h6
-            pc[0] += 1
-        if h6.isdigit():
-            h6 = int(h6)
-            if h6 < PWLEN and p2[h6] == PH_CHAR:
-                p2[h6] = h7
-                pc[1] += 1
-    if pc[0] == PWLEN and pc[1] == PWLEN:
-        mpc.keep_popping = False
-    if verbose:
-        print('\rHacking... ' + ''.join(p1) + ' ' + ''.join(p2), end='')
 
 
 @blitzen.run
 def main(input_string, verbose=False):
     seed = input_string
-    p1, p1c = [PH_CHAR] * PWLEN, 0
-    p2, p2c = [PH_CHAR] * PWLEN, 0
-    pc = [0, 0]
-    multiproc.pool(proc_fun=hash_filter, proc_args=(seed,),
-                   post_fun=pw_fill, post_args=(p1, p2, pc, verbose))
-    p1 = ''.join(p1)
-    p2 = ''.join(p2)
-    if verbose:
-        print('\rHacked!:   ' + p1 + ' ' + p2)
+    p2 = {}
+    p1 = []
+    i = 0
+    while True:
+        hash = md5hash(seed + i.__str__())
+        if hash[:5] == '00000':
+            key = hash[5]
+            p1.append(key)
+            if key.isdigit() and key not in p2 and 0 <= int(key) <= 7:
+                p2[key] = hash[6]
+                if len(p2) == 8:
+                    break
+        i += 1
+    p2 = ''.join(p2[c] for c in sorted(p2))
+    p1 = ''.join(p1[:8])
     return p1, p2
-
--- AoC_2016/AoC_2016_14.py
+++ AoC_2016/AoC_2016_14.py
@@ -1,9 +1,6 @@
 import blitzen
 from donner.misc import md5hash
-from donner import multiproc
 import re
-
-THOUSAND = 1000
 
 
 def stretch(seed):
@@ -13,41 +10,26 @@ def stretch(seed):
     return ret
 
 
-def re_hash(h):
-    cm = re.search(r'((.)\2\2)', h)
-    if cm is not None:
-        c3 = cm.group(2)
-        c5 = tuple(i for _, i in re.findall(r'((.)\2\2\2\2)', h))
-        return c3, c5
-    else:
-        return False, tuple()
-
-
-def hash_and_check(start, batch_size, hashfun, salt):
-    hashes = [(i, hashfun(salt + str(i))) for i in range(start, start + batch_size)]
-    c35 = [(i, re_hash(h)) for i, h in hashes]
-    c35 = [(i, c3, c5) for i, (c3, c5) in c35 if c3]
-    return c35
-
-
-def find_keys(mpc, result, keys, triples, want):
-    for j, c3, c5 in result:
-        if c3:
-            for c in c5:
-                keys.update([key for key in triples[c] if key >= j - THOUSAND])
-                triples[c].clear()
-                if mpc.app_limit is None and len(keys) >= want:
-                    mpc.app_limit = max(max(x) for x in triples.values() if x) + THOUSAND
-            triples[c3].add(j)
-
-
 def generate(salt, hashfun, want):
     keys = set()
     triples = {c: set() for c in '0123456789abcdef'}
-    multiproc.pool(proc_fun=hash_and_check, proc_args=(hashfun, salt),
-                   post_fun=find_keys, post_args=(keys, triples, want))
-    keys = tuple(sorted(keys))
-    return keys[want - 1]
+    triple_pattern = re.compile(r'(.)\1\1')
+    quint_pattern = re.compile(r'(.)\1\1\1\1')
+    i = 0
+    key = None
+    while key is None or i <= key + 1000:
+        hash = hashfun(salt + str(i))
+        if match := triple_pattern.search(hash):
+            for c in quint_pattern.findall(hash):
+                for j in triples[c]:
+                    if 0 < i - j <= 1000:
+                        keys.add(j)
+                        if len(keys) == want:
+                            key = max(keys)
+                triples[c].clear()
+            triples[match.group(1)].add(i)
+        i += 1
+    return sorted(keys)[want-1]
 
 
 @blitzen.run
